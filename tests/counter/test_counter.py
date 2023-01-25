b0b06da7179b78ff33c43def3e0b8cd2c930fa05
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2023-01-25 14:12:16 -0300

    FEAT: counter test

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,14 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    pythonOcurrencesUpper = count_ocurrences("data/jobs.csv", "Python")
+    pythonOcurrencesLower = count_ocurrences("data/jobs.csv", "Python")
+    javascriptOcurrencesUpper = count_ocurrences("data/jobs.csv", "Javascript")
+    javascriptOcurrencesLower = count_ocurrences("data/jobs.csv", "Javascript")
+
+    assert (pythonOcurrencesUpper == pythonOcurrencesLower)
+    assert (javascriptOcurrencesUpper == javascriptOcurrencesLower)
+
+    assert(pythonOcurrencesUpper == 1639)
+    assert(javascriptOcurrencesUpper == 122)
